d82a1e264f1b7d32879ae2434b7078297263ec91
kcroo <kirstencorrao@gmail.com>
2020-07-25 14:25:45 -0700

    displays day of the week

--- src/app/components/location-forecast/location-forecast.component.ts
+++ src/app/components/location-forecast/location-forecast.component.ts
@@ -2,6 +2,16 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ForecastFiveDay } from '../models/APIforecastFiveDay.model'
 import { ForecastDaily } from '../models/APIforecastDaily.model'
 
+const daysOfWeek = {
+  0: 'Sun',
+  1: 'Mon',
+  2: 'Tue',
+  3: 'Wed',
+  4: 'Thu',
+  5: 'Fri',
+  6: 'Sat'
+}
+
 @Component({
   selector: 'app-location-forecast',
   templateUrl: './location-forecast.component.html',
@@ -20,6 +30,12 @@ export class LocationForecastComponent implements OnInit {
   getMonthAndDate(unixTime: number): string {
     const date = new Date(unixTime * 1000);
     return `${date.getMonth()}/${date.getDate()}`
+  }
+
+  getDayOfWeek(unixTime: number): string {
+    const date = new Date(unixTime * 1000);
+    const weekdayNum = date.getDay();
+    return daysOfWeek[weekdayNum];
   }
 
   roundNumber(num: number) {
